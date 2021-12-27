750c08338720cc711f22b29ff720dbe69045e654
Arnaud Pion <apion@aviatrix.com>
2021-12-27 14:31:08 +0100

    Version 1 : MVP

--- v1/main.tf
+++ v1/main.tf
@@ -1,3 +1,4 @@
+# Deploy 2 VPC’s named vpc1 and vpc2 
 resource "aws_vpc" "vpc1" {
   cidr_block = "10.1.0.0/16"
   tags = {
@@ -12,7 +13,8 @@ resource "aws_vpc" "vpc2" {
   }
 }
 
-resource "aws_route_table" "rt_vpc1" {
+# Deploy a routing table in each VPC
+resource "aws_route_table" "vpc1_rt" {
   vpc_id = aws_vpc.vpc1.id
   route {
     cidr_block                = "10.2.0.0/16"
@@ -23,7 +25,7 @@ resource "aws_route_table" "rt_vpc1" {
   }
 }
 
-resource "aws_route_table" "rt_vpc2" {
+resource "aws_route_table" "vpc2_rt" {
   vpc_id = aws_vpc.vpc2.id
   route {
     cidr_block                = "10.1.0.0/16"
@@ -34,6 +36,7 @@ resource "aws_route_table" "rt_vpc2" {
   }
 }
 
+# Deploy 2 subnets in each VPC named <vpcname>-subnet1 and <vpcname>-subnet2
 resource "aws_subnet" "vpc1_subnet1" {
   vpc_id     = aws_vpc.vpc1.id
   cidr_block = "10.1.1.0/24"
@@ -66,6 +69,28 @@ resource "aws_subnet" "vpc2_subnet2" {
   }
 }
 
+# Attach (associate) the subnets to the created routing table
+resource "aws_route_table_association" "vpc1_subnet1_rt_asso" {
+  subnet_id      = aws_subnet.vpc1_subnet1.id
+  route_table_id = aws_route_table.vpc1_rt.id
+}
+
+resource "aws_route_table_association" "vpc1_subnet2_rt_asso" {
+  subnet_id      = aws_subnet.vpc1_subnet2.id
+  route_table_id = aws_route_table.vpc1_rt.id
+}
+
+resource "aws_route_table_association" "vpc2_subnet1_rt_asso" {
+  subnet_id      = aws_subnet.vpc2_subnet1.id
+  route_table_id = aws_route_table.vpc2_rt.id
+}
+
+resource "aws_route_table_association" "vpc2_subnet2_rt_asso" {
+  subnet_id      = aws_subnet.vpc2_subnet2.id
+  route_table_id = aws_route_table.vpc2_rt.id
+}
+
+# Peer the VPC’s together
 resource "aws_vpc_peering_connection" "vpc1_to_vpc2_peering" {
   peer_vpc_id = aws_vpc.vpc2.id
   vpc_id      = aws_vpc.vpc1.id
