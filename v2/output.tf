c4bb11a024710064bd7aaf2ae85d12a0b448b7aa
Arnaud Pion <apion@aviatrix.com>
2021-12-28 18:52:24 +0100

    Version 2 : add EC2 instances

--- v2/output.tf
+++ v2/output.tf
@@ -1,10 +1,3 @@
-/*
-output "subnet_list" {
-  description = "List of subnets created"
-  value       = [aws_subnet.vpc1_subnet1, aws_subnet.vpc1_subnet2, aws_subnet.vpc2_subnet1, aws_subnet.vpc2_subnet1]
-}
-*/
-
 output "ec2_info" {
   description = "Instances created with their public and private IP addresses"
   value = tomap({
